bee685a7e320e2d13b324c0ad3aa1550b8517759
Aleksa Zivkovic <aleksa.zivkovic@toppandigital.com>
2023-11-21 14:27:24 +0100

    Adding DAO calls to the services(all of them)

--- src/main/java/in/msproject1/schoolscheduler/schedulerAPI/service/Grupa/GrupaService.java
+++ src/main/java/in/msproject1/schoolscheduler/schedulerAPI/service/Grupa/GrupaService.java
@@ -1,34 +1,65 @@
 package in.msproject1.schoolscheduler.schedulerAPI.service.Grupa;
 
+import in.msproject1.schoolscheduler.schedulerAPI.DAO.Grupa.IGrupaDAO;
 import in.msproject1.schoolscheduler.schedulerAPI.model.Grupa;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class GrupaService implements IGrupaService{
+
+    @Autowired
+    private IGrupaDAO grupaDAO;
+
     @Override
     public List<Grupa> GetGrupaTables() {
-        return null;
+        try{
+            return grupaDAO.GetGrupaAll();
+        }catch (Exception e)
+        {
+            throw e;
+        }
     }
 
     @Override
     public Grupa GetGrupa(int id) {
-        return null;
+        try{
+            return grupaDAO.GetGrupaSingle(id);
+        }catch (Exception e)
+        {
+            throw e;
+        }
     }
 
     @Override
     public Grupa saveGrupa(Grupa gr) {
-        return null;
+        try{
+            return grupaDAO.saveGrupa(gr);
+        }catch (Exception e)
+        {
+            throw e;
+        }
     }
 
     @Override
     public Boolean deleteGrupa(int id) {
-        return null;
+        try{
+            return grupaDAO.deleteGrupa(id);
+        }catch (Exception e)
+        {
+            throw e;
+        }
     }
 
     @Override
     public Grupa updateGrupa(Grupa gr) {
-        return null;
+        try{
+            return grupaDAO.saveGrupa(gr);
+        }catch (Exception e)
+        {
+            throw e;
+        }
     }
 }
--- src/main/java/in/msproject1/schoolscheduler/schedulerAPI/service/Ucionica/IUcionicaService.java
+++ src/main/java/in/msproject1/schoolscheduler/schedulerAPI/service/Ucionica/IUcionicaService.java
@@ -1,7 +1,9 @@
 package in.msproject1.schoolscheduler.schedulerAPI.service.Ucionica;
 
+import in.msproject1.schoolscheduler.schedulerAPI.DAO.Ucionica.IUcionicaDAO;
 import in.msproject1.schoolscheduler.schedulerAPI.model.Tester;
 import in.msproject1.schoolscheduler.schedulerAPI.model.Ucionica;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
